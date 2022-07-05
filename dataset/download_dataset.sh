f21e072ff09298f34605f1cc68f8aed41f84e037
Juan Rey <jsreyl@udel.edu>
2022-07-05 15:11:05 -0400

    07/05 Fixing filenames for dataset download

--- dataset/download_dataset.sh
+++ dataset/download_dataset.sh
@@ -2,7 +2,7 @@
 
 #Create list of file ids and file names for downloading from google-drive
 file_ids=("1qz7WBKaxwPm2MPn5e98Zyp3qw5dH3drh" "1p63sWu0YDUXzGC7fVtF3EsSYcrfYAQAQ")
-file_names=("rcnn_dataset_full.zip"    "backbone_dataset.zip")
+file_names=("backbone_dataset.zip"  "rcnn_dataset_full.zip")
 
 # download each file via wget
 for i in ${!file_ids[@]};
